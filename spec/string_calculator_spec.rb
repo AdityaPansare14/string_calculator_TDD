955cc531b6fd742737404a651d534b4bda10390b
Adityapansare14 <adityapansare2000@gmail.com>
2025-07-10 07:13:46 +0530

    Case 4: Handle new lines and support different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -34,5 +34,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.new.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when custom delimiter is specified' do
+      it 'returns the sum using the custom delimiter' do
+        expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
